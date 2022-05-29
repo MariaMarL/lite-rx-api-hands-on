dd9b93949d139781a1c2785f00b1b5000f738217
Maria del Mar Lamilla <lamilla767@gmail.com>
2022-05-29 10:09:23 -0500

    Solved until exercise number 9

--- src/main/java/io/pivotal/literx/Part07Errors.java
+++ src/main/java/io/pivotal/literx/Part07Errors.java
@@ -31,16 +31,19 @@ public class Part07Errors {
 
 //========================================================================================
 
-	// TODO Return a Mono<User> containing User.SAUL when an error occurs in the input Mono, else do not change the input Mono.
+	// TODO Return a Mono<User> containing User.SAUL when an error occurs in the input Mono,
+	//  else do not change the input Mono.
 	Mono<User> betterCallSaulForBogusMono(Mono<User> mono) {
-		return null;
+		return mono.onErrorReturn(User.SAUL);
 	}
 
 //========================================================================================
 
-	// TODO Return a Flux<User> containing User.SAUL and User.JESSE when an error occurs in the input Flux, else do not change the input Flux.
+	// TODO Return a Flux<User> containing User.SAUL and User.JESSE when an error occurs
+	//  in the input Flux, else do not change the input Flux.
 	Flux<User> betterCallSaulAndJesseForBogusFlux(Flux<User> flux) {
-		return null;
+
+		return flux.onErrorResume(x -> Flux.just(User.SAUL, User.JESSE));
 	}
 
 //========================================================================================
@@ -48,8 +51,16 @@ public class Part07Errors {
 	// TODO Implement a method that capitalizes each user of the incoming flux using the
 	// #capitalizeUser method and emits an error containing a GetOutOfHereException error
 	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.map(user -> {
+			try{
+				return capitalizeUser(user);
+			}
+			catch (GetOutOfHereException e){
+				throw Exceptions.propagate(e);
+			}
+		});
 	}
+
 
 	User capitalizeUser(User user) throws GetOutOfHereException {
 		if (user.equals(User.SAUL)) {
@@ -57,6 +68,7 @@ public class Part07Errors {
 		}
 		return new User(user.getUsername(), user.getFirstname(), user.getLastname());
 	}
+
 
 	protected final class GetOutOfHereException extends Exception {
 	    private static final long serialVersionUID = 0L;
